811e26cf0274a59f951670ac482788d7b54c819b
victorze <victorzevallos@protonmail.com>
2019-03-12 12:32:15 -0500

    to lower

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const btnUpper = document.getElementById('upper');
+const btnLower = document.getElementById('lower');
 const copy = document.getElementById('copy');
 
 btnUpper.addEventListener('click', () => {
     const textarea = document.querySelector('textarea')
     let text = textarea.value;
     textarea.value = text.toUpperCase();
+});
+
+btnLower.addEventListener('click', () => {
+    const textarea = document.querySelector('textarea')
+    let text = textarea.value;
+    textarea.value = text.toLowerCase();
 });
 
 copy.addEventListener('click', () => {
